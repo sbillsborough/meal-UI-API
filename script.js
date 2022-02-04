c220c1076ad38a5d8649f882bfa824788906f8fb
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-02-04 12:22:09 +0000

    added fetch request and removed styling

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const search_meal = 'http://www.themealdb.com/api/json/v1/1/search.php?s="';
+const search = document.getElementById("search");
+const mealsEl = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const single_mealEl = document.getElementById("single-meal");
 
-const meal_img =
-  "https://themealdb.com/images/media/meals/llcbn01574260722.jpg/preview";
+function searchMeal(e) {
+  e.preventDefault();
+  // clears the single meal
+  single_mealEl.innerHTML = "";
+  // get the search term
+  const searchTerm = search.value;
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
+  if (searchTerm.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+      .then((res) => res.json())
+      .then((data) => {
+        resultHeading.innerHTML = `<h3>You searched for '${searchTerm}'<h3>`;
+        if (data.meals === null) {
+          resultHeading.innerHTML = `Something went wrong. Please try again!`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meals) => `<div class="meal">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <div class="meal-info" data-mealID="${meal.idMeal}">
+              <h3>${meal.strMeal}</h3>
+            </div>
+            </div>
+            `
+            )
+            .join("");
+        }
+      });
+    search.value = "";
+  } else {
+    resultHeading.innerHTML = "<h2>Please type in the keywords to search</h2>";
+  }
+}
 
-// Fetch initial meals
-
-getMeals(search_meal) {
-    const response = await fetch(url);
-    if (response.status === 200) {
-    const data = await response.json();
-    }
-
-    showMeals(data.results);
+// get meal by id
+function getMealByID(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDom(meal);
+    });
 }
